ad56e8614fd9bbcd7fca7a0807735d97bc848c7b
nova557 <71886498+nova557@users.noreply.github.com>
2022-03-24 14:17:32 -0400

    Create Code

---
+++ index.ts
@@ -0,0 +1,21 @@
+import DiscordJs, { Intents } from "discord.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const client = new DiscordJs.Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
+
+client.on("ready", () => {
+  console.log("The bot is ready");
+});
+
+client.on("messageCreate", (message) => {
+  if (message.content === "ping") {
+    message.reply({
+      content: "pong",
+    });
+  }
+});
+
+client.login(process.env.TOKEN);
